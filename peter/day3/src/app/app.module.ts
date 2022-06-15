c0f255d54e703fa47fb464b3f3dd47fe52b4d5e8
Peter Aigner <mastapetz@icloud.com>
2022-06-15 10:53:56 +0200

    create new components, add first data

--- peter/day3/src/app/app.module.ts
+++ peter/day3/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
